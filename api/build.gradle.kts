7a4aa6f035cc683cf69fbedffd5f271180b968c8
Yannick Lamprecht <yannicklamprecht@live.de>
2023-04-23 15:42:21 +0200

    fix and reduce build file duplicates

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,32 +1,26 @@
 plugins {
-	`java-library`
+    `java-library`
 }
 
 group = "me.delta.mc.marker"
 version = "1.0-SNAPSHOT"
 description = "MarkerAPI"
 
-repositories {
-	mavenCentral()
-	maven("https://repo.papermc.io/repository/maven-public/")
-	maven("https://oss.sonatype.org/content/groups/public/")
-	maven("https://repo.maven.apache.org/maven2/")
-}
-
 dependencies {
-	compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
-}
+    val paper = "io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT"
+    val snakeyaml = "org.yaml:snakeyaml:2.0"
 
-java {
-	toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
+    compileOnly(paper) {
+        exclude("org.yaml", "snakeyaml")
+        because("Snakeyaml 1.33 has a vulnerability.")
+    }
 
-tasks {
-	withType<JavaCompile>() {
-		options.encoding = "UTF-8"
-	}
+    compileOnly(snakeyaml) {
+        because("We need to replace the removed vulnerable dependency")
+    }
 
-	withType<Javadoc>() {
-		options.encoding = "UTF-8"
-	}
+    testCompileOnly(paper) {
+        exclude("org.yaml", "snakeyaml")
+    }
+    testCompileOnly(snakeyaml)
 }
--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -9,35 +9,12 @@ group = "me.delta.mc.marker"
 version = "1.0-SNAPSHOT"
 description = "MarkerAPI"
 
-repositories {
-	mavenCentral()
-	maven("https://repo.papermc.io/repository/maven-public/")
-	maven("https://oss.sonatype.org/content/groups/public/")
-	maven("https://repo.maven.apache.org/maven2/")
-}
-
 dependencies {
 	compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 	implementation(project(":api"))
 }
 
-java {
-	toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
 tasks {
-	withType<JavaCompile>() {
-		options.encoding = "UTF-8"
-	}
-
-	withType<Javadoc>() {
-		options.encoding = "UTF-8"
-	}
-
-	runServer {
-		minecraftVersion("1.19.4")
-	}
-
 	bukkit {
 		name = "Marker-API"
 
